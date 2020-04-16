a60fe5390f0da0377caef969857849656c734037
eliton gomes <eliton_engenharia@yahoo.com.br>
2020-04-16 17:57:45 -0300

    curso javascript

---
+++ ex-012.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(` a velocidade do seu carro e ${vel}km/h`)
+if(vel > 60){ //condicao simples 
+    console.log(`voce ultrapasou a velocidade permitida multado`)
+}
+
+console.log(`dirija sempre `)
